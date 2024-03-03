229d89051f45c35ba49c2349ee0619c0507541d7
anuj bhandari <anujb364@gmail.com>
2024-03-03 09:51:54 +0545

    My cake website

---
+++ script/products.js
@@ -0,0 +1,29 @@
+export const products = [
+    {
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      image: "images/images/cake1.png",
+      name: "Cake1",
+      priceCents: 1090,
+      keywords: [
+        "chocolate"
+      ]
+    },
+    {
+     id: "e43638ce-6aasdfcasdghasdf",
+      image: "images/images/cake2.png",
+      name: "Cake2",
+      priceCents: 1080,
+      keywords: [
+        "vanilla"
+      ]
+    },
+    {
+        id: "e4363asdgacnanafhadsfkfj",
+         image: "images/images/cake3.png",
+         name: "Cake3",
+         priceCents: 2000,
+         keywords: [
+           "red velvet"
+         ]
+       },
+]
